fad5237d22f4a729a0c7925811d44b8ad04203e7
apokalipsa44 <m_lachowicz@wp.pl>
2019-11-06 00:03:42 +0100

    part 2

--- buttons/src/main/java/com/michau/buttons/MainActivity.kt
+++ buttons/src/main/java/com/michau/buttons/MainActivity.kt
@@ -2,11 +2,25 @@ package com.michau.buttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        lke.setOnClickListener{
+            var message= Toast.makeText(applicationContext, "ssij", Toast.LENGTH_SHORT)
+            message.show()
+        }
+
+        disl.setOnClickListener{
+            var message=Toast.makeText(applicationContext, "nie ssij", Toast.LENGTH_LONG)
+            message.show()
+
+            
+        }
     }
 }
