902c0dea56d98eb11a402bfe8b3d196bd221356d
apokalipsa44 <m_lachowicz@wp.pl>
2019-11-07 00:21:22 +0100

    #7

--- buttons/src/main/java/com/michau/buttons/Main2Activity.kt
+++ buttons/src/main/java/com/michau/buttons/Main2Activity.kt
@@ -2,11 +2,24 @@ package com.michau.buttons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    // coś jak render z reacta
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        if (passw.isFocused) {
+            if (passw.length() < 6) {
+                passwVerify.setText("to short")
+            }else{
+                passwVerify.setText("")
+            }
+        }
     }
 }
